b3a102af2105457bcd7e5134d462bb61597fa972
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-03-08 19:32:30 -0300

    Adiciona PedidoService com operações de CRUD

---
+++ lavanderia-spring/src/main/java/leandro/dev/service/PedidoService.java
@@ -0,0 +1,34 @@
+package leandro.dev.service;
+
+
+import leandro.dev.exception.RecursoNaoEncontradoException;
+import leandro.dev.model.Pedido;
+import leandro.dev.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PedidoService {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    public List<Pedido> listarPedidos() {
+        return pedidoRepository.findAll();
+    }
+
+    public Pedido criarPedido(Pedido pedido) {
+        return pedidoRepository.save(pedido);
+    }
+
+    public Pedido buscarPedidoPorId(Long id) {
+        return pedidoRepository.findByIdWithCliente(id)
+                .orElseThrow(() -> new RecursoNaoEncontradoException("Pedido não encontrado"));
+    }
+
+    public void excluirPedido(Long id) {
+        pedidoRepository.deleteById(id);
+    }
+}
