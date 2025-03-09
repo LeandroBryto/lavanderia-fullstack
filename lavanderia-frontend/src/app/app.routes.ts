0d1d9a90f04844fe01487f12e4502abd6bf9e17b
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-03-08 22:43:23 -0300

    Configura rotas para navegação

--- lavanderia-frontend/src/app/app.routes.ts
+++ lavanderia-frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListarPedidosComponent } from './pages/listar-pedidos/listar-pedidos.component';
+import { CriarPedidoComponent } from './pages/criar-pedido/criar-pedido.component';
+import { EditarPedidoComponent } from './pages/editar-pedidos/editar-pedidos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/pedidos', pathMatch: 'full' }, // Rota padrão
+  { path: 'pedidos', component: ListarPedidosComponent }, // Listar pedidos
+  { path: 'pedidos/novo', component: CriarPedidoComponent }, // Criar pedido
+  { path: 'pedidos/editar/:id', component: EditarPedidoComponent }, // Editar pedido
+  { path: '**', redirectTo: '/pedidos' } // Rota curinga (404)
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
